52116c744bd4fe897efc973a91813b8f2a8ed228
Anton <52106716+Xebit8@users.noreply.github.com>
2024-08-07 20:02:59 +0300

    basic score structure

---
+++ Reaction Trainer/Score.cs
@@ -0,0 +1,24 @@
+﻿namespace Reaction_Trainer
+{
+    public class Score
+    {
+        private int score = 0;
+        private double avgTime = -1;
+        public void setScore(int user_score)
+        {
+            score = user_score;
+        }
+        public void setAvgTime(double user_avgTime)
+        {
+            avgTime = user_avgTime;
+        }
+        public int getScore()
+        {
+            return score;
+        }
+        public double getAvgTime()
+        {
+            return avgTime;
+        }
+    }
+}
