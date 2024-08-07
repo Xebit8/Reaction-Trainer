e029f3c5ef92672280cf410b3d4fd5382bbdb7e4
Anton <52106716+Xebit8@users.noreply.github.com>
2024-08-07 20:02:00 +0300

    general Mark structure + 1 hit mark type

---
+++ Reaction Trainer/Mark.cs
@@ -0,0 +1,46 @@
+﻿namespace Reaction_Trainer
+{
+    public class Mark
+    {
+        private int health;
+        private Point position;
+        private Size size;
+
+        public Mark(Point position)
+        {
+            this.position = position;
+        }
+        public void getDamage(int damage)
+        {
+            health -= damage;
+        }
+        public void changePosition()
+        {
+            Random rnd = new Random();
+            position = new Point(rnd.Next(), rnd.Next());
+        }
+        public int getHealth()
+        {
+            return health;
+        }
+        public Size getSize()
+        {
+            return size;
+        }
+        public Point getPosition()
+        {
+            return position;
+        }
+    }
+    public class EasyMark : Mark
+    {
+        private int health = 1;
+        private Size size = new Size(10, 10);
+        private Point position;
+
+        public EasyMark(Point position) : base(position)
+        {
+            this.position = position;
+        }
+    }
+}
